0003494ec526ae4ab0ee5c4a415343fa1d2ea8d5
rkumar021 <68196031+rkumar021@users.noreply.github.com>
2020-07-16 22:30:59 +0530

    modelserializer added

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from core.views import BookList
+from core.views import BookList, Bookcreate, Bookdestroy
 
 urlpatterns = [
     path('api/books',BookList.as_view()),
-    
+    path('api/books/new', Bookcreate.as_view()),
+    path('api/books/<int:id>/delete', Bookdestroy.as_view())
 ]
 
 #
