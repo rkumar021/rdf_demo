09cad3f4326289e466e1ede1925e094bf21ee1c5
rkumar021 <68196031+rkumar021@users.noreply.github.com>
2020-07-17 15:15:34 +0530

    CRUD operations

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from core.views import BookList, Bookcreate, Bookdestroy
+from core.views import BookList, Bookcreate, Bookupdatedestroy
 
 urlpatterns = [
     path('api/books',BookList.as_view()),
     path('api/books/new', Bookcreate.as_view()),
-    path('api/books/<int:id>/delete', Bookdestroy.as_view())
+    path('api/books/<int:id>', Bookupdatedestroy.as_view())
 ]
 
 #
--- core/views.py
+++ core/views.py
@@ -2,7 +2,8 @@
 # from rest_framework import Listview
 # from rest_framework import serializers
 from rest_framework import serializers
-from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView
+from rest_framework import response
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
 #from rest_framework.response import Response
 
 from core.models import Book
@@ -19,12 +20,28 @@ class Bookcreate(CreateAPIView):
     def create(self, request, *args, **kwargs ):
         return super().create(request, *args, **kwargs)
 
-class Bookdestroy(DestroyAPIView):
+class Bookupdatedestroy(RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
+    serializer_class = BookSerializer
     lookup_field = 'id'
 
     def delete(self, request, *args, **kwargs ):
-        return super().delete(request, *args, **kwargs)
-        #if response.status_code == 204
-        #     from django.core.cache import cache
-        #     cache.delete(Book_data)
+        response = super().delete(request, *args, **kwargs)
+        Book_id = request.data.get('id')
+        if response.status_code == 204:
+            from django.core.cache import cache
+            cache.delete('Book_data_{}'.format(Book_id))
+        return response
+    def update(self, request, *args, **kwargs ):
+        response = super().update(request, *args, **kwargs)
+        if response.status_code == 200:
+            from django.core.cache import cache
+            Book = response.data
+            cache.set('Book_data_{}'.format(Book['id']),{
+                'created': Book['created'],
+                'title': Book['title'],
+                'description': Book['description'],
+                'price': Book['price'] 
+            })
+        return response
+
