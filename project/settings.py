af84459563b427e066341aed28a1e67b8652f187
rkumar021 <68196031+rkumar021@users.noreply.github.com>
2020-07-18 16:49:33 +0530

    auth added

--- project/settings.py
+++ project/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'core',
+    'rest_framework.authtoken',
+    'rest_auth',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- project/urls.py
+++ project/urls.py
@@ -18,5 +18,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("core.urls"))
+    path("",include("core.urls")),
+    path('rest-auth/', include('rest_auth.urls')),
+    
+    
+    #path('rest-auth/registration/', include('rest_auth.registration.urls'))
 ]
