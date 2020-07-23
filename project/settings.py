120f5afecd4614e875d1e0bc13a02fdf9c100bcf
rkumar021 <68196031+rkumar021@users.noreply.github.com>
2020-07-23 23:22:00 +0530

    change in settings

--- project/settings.py
+++ project/settings.py
@@ -115,7 +115,7 @@ REST_FRAMEWORK = {
 #     'PAGE_SIZE': 10
         'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        #'rest_framework.renderers.BrowsableAPIRenderer',
     ]
  }
 
